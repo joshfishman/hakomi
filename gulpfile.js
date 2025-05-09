3bd076a9d2094407dca18f9cdb1f8472a65ef154
Joshua Fishman <thejoshfishman@gmail.com>
2025-05-09 17:51:09 +0000

    Changed Dates and Header Links

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,27 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const shell = require('gulp-shell');
 
-// Jekyll build task
-gulp.task('jekyll', shell.task(['bundle exec jekyll build --watch']));
+// Simplified the Jekyll task to avoid TypeError
+gulp.task('jekyll', shell.task(['bundle exec jekyll build --watch'], { verbose: true }));
 
-// BrowserSync task
-gulp.task('serve', function() {
+// BrowserSync task with improved logging
+gulp.task('serve', function () {
   browserSync.init({
     server: {
       baseDir: "./_site"
-    }
+    },
+    notify: false, // Disable BrowserSync notifications
+    open: false    // Prevent BrowserSync from opening a browser window automatically
   });
-  // Watch for Jekyll rebuilds
-  gulp.watch("_site/**/*.*").on('change', browserSync.reload);
+
+  console.log('BrowserSync is running. Watching for file changes...');
+
+  // Watch for Jekyll rebuilds and reload BrowserSync
+  gulp.watch("_site/**/*.*").on('change', function (file) {
+    console.log(`File changed: ${file}`);
+    browserSync.reload();
+  });
 });
 
-// Default task
+// Default task with parallel execution
 gulp.task('default', gulp.parallel('jekyll', 'serve'));
